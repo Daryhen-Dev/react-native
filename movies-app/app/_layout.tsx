1cd9987cee1d0c35662f0eccf328000221b3dc06
Daryhen-dev <daryhen.work@gmail.com>
2025-03-11 16:58:01 -0500

    create movie app

---
+++ movies-app/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import '../global.css'
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text className='text-3xl'>RootLayout</Text>
+    </View>
+  )
+}
+
+export default RootLayout
